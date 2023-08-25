c81dc1ed08528817166796fa29442096afd24fb6
Misikir Abate <Misikir.A@MACROGC.LOCAL>
2023-08-25 14:16:42 +0300

    The component branch with the following featues

---
+++ math-magic/src/components/Calculator.js
@@ -0,0 +1,12 @@
+import './css/Calculator.css';
+import Child from './CalculatorChild';
+
+const Component = () => (
+  <div className="cal-contianer">
+    <div className="diplay" data-testid="result">
+      0
+    </div>
+    <Child />
+  </div>
+);
+export default Component;
