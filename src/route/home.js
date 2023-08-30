c9638cbc6a4921768a85be0d47609be1d7fb5704
misikira <misikirteka@gmail.com>
2023-08-30 19:09:21 +0300

    fix issues

--- src/route/home.js
+++ src/route/home.js
@@ -12,8 +12,6 @@ const HomePage = () => (
         ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       {' '}
     </div>
@@ -26,8 +24,6 @@ const HomePage = () => (
         ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       {' '}
     </div>
